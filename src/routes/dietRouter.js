3ec918ec7978874b55d3665deb915f861e16cc9e
daelflodo <davidflodo@gmail.com>
2023-08-11 11:29:09 -0500

    delete

--- src/routes/dietRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express')
-const dietRouter = Router();
-const  { getDietByIdHandler, getDietHandler } = require('../handles/dietHandlers')
-// const router = require('express').Router();
-
-dietRouter.get('/',getDietHandler)
-// dietRouter.get('/:id', getDietByIdHandler)
-
-module.exports = dietRouter
